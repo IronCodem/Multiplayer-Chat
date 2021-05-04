d097924a98a4610baae6f200e8f5dd90eb58419c
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-04 12:09:21 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
 const io = require('socket.io-client');
 const readline = require('readline');
 
-var socket = io("https://repl-chat-server.<your username>.repl.co");
+var socket = io("https://repl-chat-server.yashasshah.repl.co");
 
 const chat_interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+var colors = require('colors/safe');
+colors.setTheme({
+  input: 'grey',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
 
 var chat_handle = "";
 var message_to_send = "";
@@ -18,16 +29,14 @@ socket.on('connect', function(){
     socket.on('broadcast', display_message);
 });
 
-// Gets the user's name, so we can introduce and append each message with their name
 function get_chat_handle(){
-    chat_interface.question(`Hello! What's your chat handle? `, function(answer){
+    chat_interface.question(colors.prompt(`Hello! What's your username? `), function(answer){
         chat_handle = answer;
-        socket.emit("message", chat_handle + ' has entered the chat');
+        socket.emit("message", chat_handle + (colors.info(' has entered the chat')));
         chat();
     });
 }
 
-// Waits for a new message to send
 function chat(){
     chat_interface.question(chat_handle + ": ", function(message){
         message_to_send = chat_handle + ': ' + message;
@@ -36,12 +45,11 @@ function chat(){
     });
 }
 
-// Handles an incoming message, and checks to see that it is not the one we sent.
-// Shows it on the console if it is from another user.
 function display_message(message){
     if(message_to_send != message){
         console.log("\n" + message);
         chat();
     }
 }
+
 
